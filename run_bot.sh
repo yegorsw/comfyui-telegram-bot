5837a54387525c98080fa216eac3f03ea6162731
yegorsw <yegorsw@gmail.com>
2024-09-14 12:15:04 -0700

    simpler logging

--- run_bot.sh
+++ run_bot.sh
@@ -10,7 +10,7 @@ cd "$SCRIPT_DIR"
 source ./.venv/bin/activate 
 
 # Run the Python script and redirect the output to the log file
-python3 -u ./main.py 2>&1 | tee log_temp.log
+python3 -u ./main.py 2>&1 | tee logs/log_$(date +"%Y-%m-%d_%H-%M-%S").log
 
 # Clean the log file to remove control characters and overwrite it
-col -b < log_temp.log > logs/log_$(date +"%Y-%m-%d_%H-%M-%S").log
+# col -b < log_temp.log > logs/log_$(date +"%Y-%m-%d_%H-%M-%S").log
